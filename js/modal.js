52a2e2ca723ac3bbbeef46625e12a2aaafe08db2
DrunkCherry1997 <142116152+Marchenko1997@users.noreply.github.com>
2024-02-17 22:57:54 +0100

    Adding JS

---
+++ js/modal.js
@@ -0,0 +1,38 @@
+(() => {
+    const refs = {
+      openModalBtn: document.querySelector(".button-type"),
+      closeModalBtn: document.querySelector(".modal-button"),
+      modal: document.querySelector(".modal-window-container"),
+    };
+  
+    if (refs.openModalBtn && refs.closeModalBtn && refs.modal) {
+      refs.openModalBtn.addEventListener("click", toggleModal);
+      refs.closeModalBtn.addEventListener("click", toggleModal);
+    }
+  
+    function toggleModal() {
+      refs.modal.classList.toggle("is-open");
+    }
+  })();
+  
+  (() => {
+    const mobileMenu = document.querySelector(".mobile-menue-window");
+    const openMenuBtn = document.querySelector(".mobile-button-open");
+    const closeMenuBtn = document.querySelector(".mobile-button-close");
+  
+    if (mobileMenu && openMenuBtn && closeMenuBtn) {
+      const toggleMenu = () => {
+        const isMenuOpen = mobileMenu.classList.contains("is-open");
+        mobileMenu.classList.toggle("is-open");
+      };
+  
+      openMenuBtn.addEventListener("click", toggleMenu);
+      closeMenuBtn.addEventListener("click", toggleMenu);
+  
+      window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
+        if (!e.matches) return;
+        mobileMenu.classList.remove("is-open");
+      });
+    }
+  })();
+  
